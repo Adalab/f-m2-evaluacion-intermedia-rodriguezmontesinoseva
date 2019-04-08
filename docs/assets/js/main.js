358b842a82c50d5f818059a786e4db288fdf2025
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-04-08 17:57:32 +0200

    add elements declaration

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -4,15 +4,30 @@
 
 // el juego genera un numero al azar entre 1 y 100. el jugador tiene que adivinarlo
 // pistas: num alto o bajo. Cuenta intentos. Hasta acierto
+
 // Jugador introduce num y pulsa Prueba. acceder a contenido del input y mostrarlo en consola. comparar numUser con el aleatorio
 // pistas en caja azul - al comparar pintar "demasiado alto", demasiado bajo" "has ganado campeona"
 // intentos en rojo, esquina sup dcha
 // Fin cuando acierta - actualizar contador de intentos cada vez que el usuario pruebe
 
+// me traigo objeto input
+
+const useNumberEl= document.querySelector('.useNumber');
+console.log(useNumberEl);
+
+
+const btnTryEl =document.querySelector('.btn-try');
+console.log(btnTryEl);
+
+function startGame(){
+
+}
+btnTryEl.addEventListener('keyup',startGame);
+
 // Función que genera un número aleatorio hasta un máximo dado
-function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-  }
+// function getRandomNumber(max) {
+//     return Math.ceil(Math.random() * max);
+//   }
   
   // Anda, chacho, píntame en la consola un número random hasta 100;
-  console.log('> ' + getRandomNumber(100));
+  // console.log('> ' + getRandomNumber(100));
